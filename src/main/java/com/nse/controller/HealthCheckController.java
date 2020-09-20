76d04a3f68f84a8e4e176ba48abdbea2a4831038
Biswajit Mohapatra <biswajit.javaexpert@gmail.com>
2020-09-21 01:12:55 +0530

    Added health controller

---
+++ src/main/java/com/nse/controller/HealthCheckController.java
@@ -0,0 +1,12 @@
+package com.nse.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+  public class HealthCheckController {
+    @GetMapping("health/status")
+    public String status(){
+      return "GREEN";
+    }
+}
